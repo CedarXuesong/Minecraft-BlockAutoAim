87a031aead1aaaf800beb84e147bd6305d4120a2
CedarXueSong <zxs17302303016@163.com>
2024-09-08 20:27:58 +0800

    新增多个功能 1.新增方块的meta数据 2.新增忽略脚下方块 3.新增指令补全

--- src/main/java/com/cedar/blockaim/Main.java
+++ src/main/java/com/cedar/blockaim/Main.java
@@ -17,7 +17,7 @@ public class Main
     private ConfigurationFile configurationFile;
     public static final String MODID = "BlockAim";
     public static final String VERSION = "1.1.6";
-    public static final String VERSIONCODE = "20240908.2025";
+    public static final String VERSIONCODE = "20240908.2027";
     public static final String CFG_FILE_PATH = "config/cedar/config.cfg";
     @EventHandler
     public void load(FMLInitializationEvent event)
